676b5b6d41a0b28f586e17fa3163e74e05c35874
Do Hyeong Kim <dhk21512@gmail.com>
2022-12-25 22:03:10 +0900

    authinit 추가

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
-import AuthInit from "./modules/Auth/AuthInit";
+import AuthInit from "../modules/Auth/AuthInit";
 import Routes from "./Routes";
 
 function App() {
   return (
     <AuthInit>
-      <Routes />;
+      <Routes />
     </AuthInit>
   );
 }
